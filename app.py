0a89020778ee8b30375dd4f8e151b2b16cb478ff
Sergejs Visockis <sergei.visotsky@gmail.com>
2025-06-28 13:41:51 +0200

    move env variables into the constants

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ import boto3
 
 client = boto3.client('sns')
 topic_arn = os.environ["TOPIC_ARN"]
+imap_server = os.environ["IPAM_SERVER"]
+imap_port = os.environ["IMAP_PORT"]
+mail_username = os.environ["USERNAME"]
+mail_password = os.environ["PASSWORD"]
 
 
 def lambda_handler(event, context):
@@ -25,10 +29,9 @@ def send_salary_notification():
 
 
 def get_invoice_data(arn, email, message):
-    mail = imaplib.IMAP4_SSL(host=os.environ["IPAM_SERVER"],
-                             port=int(os.environ["IMAP_PORT"]))
+    mail = imaplib.IMAP4_SSL(host=imap_server, port=int(imap_port))
 
-    mail.login(os.environ["USERNAME"], os.environ["PASSWORD"])
+    mail.login(mail_username, mail_password)
     mail.select(readonly=True)
     result = mail.search(None, 'From', email)
     if result is not None:
